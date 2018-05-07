6c446fa3a828ee283df80a43efc3a512e72aa5d8
Stefan Huber <hubidu@users.noreply.github.com>
2018-05-07 11:46:49 +0200

    Fix example test case

--- examples/codeceptjs/search-products_test.js
+++ examples/codeceptjs/search-products_test.js
@@ -1,12 +1,12 @@
-Feature('Search products')
+Feature('Search for Handytarife')
 
-Scenario(`When I search for "Handytarife" without specifying any details Then I should see a list of tariffs`, 
+Scenario(`When I search for "Handytarife" without specifying any details Then I will get a list of various tariffs`, 
 async (I) => {
     I.amOnPage('https://www.check24.de/handytarife')
 
-    I.click('jetzt vergleichen', '#c24-content > emotion > landingpage-filter:nth-child(4) > ng-transclude > form > div > button')
+    I.click('jetzt vergleichen', '.c24-btn-primary.c24-btn-block')
 
     I.waitInUrl('/handytarife/vergleich')
     I.see('Handytarife im Vergleich', 'h1')
-    I.seeElement('product-list product-item')
+    I.seeElement('product-item-anniversary')
 })
