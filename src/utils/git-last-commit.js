bab4090b0779bd63437e3ea1e581ad6dda2d6220
Stefan Huber <hubidu@users.noreply.github.com>
2018-07-24 13:12:00 +0200

    Fix dates in last commit data

--- src/utils/git-last-commit.js
+++ src/utils/git-last-commit.js
@@ -4,6 +4,10 @@ module.exports = () => new Promise((resolve, reject) => {
     git.getLastCommit((err, commit) => {
         if (err) return reject(err)
 
+        // Fix dates
+        commit.committedOn = Number(commit.committedOn)
+        commit.authoredOn = Number(commit.authoredOn)
+
         return resolve(commit)
     })
 })
