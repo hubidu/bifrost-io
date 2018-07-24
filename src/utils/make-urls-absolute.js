3bf9ecb84131eb0c21211f7e6e8d7e2626914281
Stefan Huber <hubidu@users.noreply.github.com>
2018-07-24 10:49:34 +0200

    Remove script tags from html snapshots

--- src/utils/make-urls-absolute.js
+++ src/utils/make-urls-absolute.js
@@ -1,5 +1,9 @@
 module.exports = (html, pageBaseUrl) => {
+    // Make all links absolute pointing to the page url
     html = html.replace(/(href="\/)([^\/])/gi, `href="${pageBaseUrl}/$2`)
     html = html.replace(/(src="\/)([^\/])/gi, `src="${pageBaseUrl}/$2`)
+
+    // Disable all script tags
+    html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
     return html
 }
